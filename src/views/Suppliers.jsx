25fa8464fc277d6ed61fc004ce5cd6325b4f4fe6
descrysama <erwing2002@gmail.com>
2023-05-18 18:49:17 +0200

    upodate

--- src/views/Suppliers.jsx
+++ src/views/Suppliers.jsx
@@ -2,23 +2,24 @@ import { Button } from 'primereact/button';
 import { runSupplierScript } from '../services/scriptService';
 import { useRef, useState } from 'react';
 import { Toast } from 'primereact/toast';
+import "file-viewer";
 
 const Suppliers = () => {
 
     const toast = useRef(null);
     const [toggle, setToggle] = useState(true);
     const [loading, setLoading] = useState(false);
-    
+
     const show = (severity, summary, message) => {
         toast.current.show({ severity: severity, summary: summary, detail: message });
     };
 
     const runSupplierRequest = () => {
-        if(toggle) {
+        if (toggle) {
             setToggle(false)
             setLoading(true)
             runSupplierScript().then((res) => {
-                if(res) {
+                if (res) {
                     show("success", "Success", res.message)
                     setToggle(true)
                     setLoading(false)
@@ -30,9 +31,13 @@ const Suppliers = () => {
 
 
     return (
-        <div className='w-full m-4 justify-center items-center'>
+        <div className='w-full flex flex-col m-4 justify-center items-center gap-2'>
             <Toast ref={toast} />
-            <Button label="Lancer le script fournisseur" icon="pi pi-check" loading={loading} onClick={() => runSupplierRequest()} />
+            <div className='flex gap-2'>
+                <Button label="Lancer le script fournisseur" icon="pi pi-check" loading={loading} onClick={() => runSupplierRequest()} />
+                <a href="http://79.137.87.52/final_output.xlsx" ><Button icon="pi pi-download" severity="success" aria-label="Search" label="Télécharger le fichier" /></a>
+            </div>
+
         </div>
     )
 }
